58e1b5f89c1a9cc1aca0adae436a55163a52d095
Abhradeep <abhradeepmandal090905@gmail.com>
2026-09-21 00:30:10 +0530

    Added get all cities api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -65,9 +65,9 @@ const get = async (req, res) => {
     }
 }
 
-const update =async (req, res) => {
+const update = async (req, res) => {
     try {
-        const response = await cityService.updateCity(req.params.id,req.body);
+        const response = await cityService.updateCity(req.params.id, req.body);
         return res.status(200).json({
             data: response,
             success: true,
@@ -76,10 +76,30 @@ const update =async (req, res) => {
         });
     } catch (error) {
         console.log("Error");
-         return res.status(500).json({
+        return res.status(500).json({
             data: {},
             success: false,
             message: "Not able to update the city",
+            err: error
+        })
+    }
+}
+
+const getAll = async (req, res) => {
+    try {
+        const cities = await cityService.getAllCities();
+        return res.status(200).json({
+            data: cities,
+            success: true,
+            message: "Successfully fetched all cities",
+            err: {}
+        });
+    } catch (error) {
+        console.log("Error");
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to get all the city",
             err: error
         })
     }
@@ -89,5 +109,6 @@ module.exports = {
     create,
     destroy,
     get,
-    update
+    update,
+    getAll
 }
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -29,12 +29,24 @@ class cityRepository {
     }
 
     async updateCity(cityId, data) { //date = object
+       // The below approach also works but will not return updated object
+            // if we are using Pg then returning: true can be used, else not
+            // const city = await City.update(data, {
+            //     where: {
+            //         id: cityId
+            //     },
+            //      
+            // });
+            // for getting updated data in mysql we use the below approach
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
+            return city;
+    }
+
+    async getCity(cityId) {
         try {
-            const city = await City.update(data, {
-                where: {
-                    id: cityId
-                }
-            });
+            const city = await City.findByPk(cityId);
             return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
@@ -42,10 +54,10 @@ class cityRepository {
         }
     }
 
-    async getCity(cityId) {
+    async getAllCities(){
         try {
-            const city = await City.findByPk(cityId);
-            return city;
+            const cities = await City.findAll();
+            return cities;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
             throw { error };
